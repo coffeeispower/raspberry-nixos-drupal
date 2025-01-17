cf2cd61751e42bb58d7389bf58ba070925f10ed9
Tiago Dinis <tiagodinis33@proton.me>
2025-01-17 16:21:08 +0000

    import my configuration to customize helix and install hx and configure git

--- flake.nix
+++ flake.nix
@@ -1,24 +1,51 @@
 {
-    nixConfig = {
-        extra-substituters =  ["https://nix-community.cachix.org"];
-        extra-trusted-public-keys = ["nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="];
-    };    
     inputs = {
-        nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
-
+        nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+        home-manager.url = "github:nix-community/home-manager";
+        home-manager.inputs.nixpkgs.follows = "nixpkgs";
         snowfall-lib = {
             url = "github:snowfallorg/lib";
             inputs.nixpkgs.follows = "nixpkgs";
         };
-        raspberry-pi-nix.url = "github:nix-community/raspberry-pi-nix/master";
+        coffeeispower-nix-config = {
+            url = "github:coffeeispower/nix-configuration";
+            inputs.nixpkgs.follows = "nixpkgs";
+            inputs.home-manager.follows = "home-manager";
+            inputs.stylix.follows = "stylix";
+        };
+        
+        drupal-website = {
+            url = "github:coffeeispower/my-drupal-site";
+            flake = false;
+        };
+        stylix = {
+          url = "github:danth/stylix/master";
+          inputs.home-manager.follows = "home-manager";
+          inputs.nixpkgs.follows = "nixpkgs";
+        };
+
     };
 
     outputs = inputs:
-        inputs.snowfall-lib.mkFlake {
-            inherit inputs;
-            channels-config = {
-                crossSystem.system = "aarch64-unknown-linux-gnu";
+        let
+            snowfall = inputs.snowfall-lib.mkLib {
+                inherit inputs;
+                src = ./.;
             };
-            src = ./.;
+        in
+        snowfall.mkFlake {
+            homes.modules = with inputs.coffeeispower-nix-config.homeModules; [
+                helix
+                nushell
+                git
+                gh
+                fastfetch
+                direnv
+                zoxide
+            ];
+            systems.modules.nixos = with inputs.coffeeispower-nix-config.nixosModules; [
+                stylix
+                inputs.stylix.nixosModules.stylix
+            ];
         };
 }
--- systems/aarch64-linux/raspberry-drupal/default.nix
+++ systems/aarch64-linux/raspberry-drupal/default.nix
@@ -1,9 +1,10 @@
-{ lib, pkgs, inputs, ... }:
+{ lib, pkgs, inputs, modulesPath, ... }:
 {
   imports = [
-    inputs.raspberry-pi-nix.nixosModules.raspberry-pi
-    inputs.raspberry-pi-nix.nixosModules.sd-image
+    "${modulesPath}/installer/sd-card/sd-image.nix"
+  ];
   nixpkgs.config.allowUnsupportedSystem = true;
+  
   # Configurar o network manager
   networking.networkmanager.enable = true;
   networking.networkmanager.dns = "none";
@@ -33,38 +34,35 @@
       "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCvBXHiSMP3IGyWxR73e8Ygo7rfT5h0zMMGKDoK8JH55FmiwolMIcqcpiLzXs4D+KogUUMtqQecMSz47ELoOn2jPdc8dT2kMtSDm3qod9MiBYg/zO4gJ0fwF2lraXCq7QVIVr9Yyjt3uIA/3vFaMSSqwAHZ5HzrvUapC32RlpUuRFpJcubcMqKal/9KfqbizboM3ppnorGpCMux9Ks+7gtw3ZCm6GM3UBX8UK4XVGeN5bzqi0CLXVM17XafPs7Ap1zPTUxLaNjSg7RRvl8Es38SvZlBnMEuyVU3GR5zLJWcTHlebrvGfj6L3aZ/mUraA5f40OW3AXeNQeX0mEq8+2cv"
     ];
   };
+  users.users.tiago = {
+    extraGroups = ["wheel" "networkmanager"];
+    isNormalUser = true;
+    initialPassword = "123456789";
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIyHhxL9uKySCBDtq2U5GlDogoZfJu28vaGb2mE8/sxP"
+    ];
+    shell = pkgs.nushell;
+  };
+  environment.shells = [ pkgs.nushell ];
   users.mutableUsers = false;
   networking.firewall = {
     enable = true;
     allowedTCPPorts = [ 80 443 22 ];
   };
-  raspberry-pi-nix.board = "bcm2711";
-  hardware = {
-    raspberry-pi = {
-      config = {
-        all = {
-          base-dt-params = {
-            BOOT_UART = {
-              value = 1;
-              enable = true;
-            };
-            uart_2ndstage = {
-              value = 1;
-              enable = true;
-            };
-          };
-          dt-overlays = {
-            disable-bt = {
-              enable = true;
-              params = { };
-            };
-          };
-        };
-      };
-    };
-  };
   system.stateVersion = "24.11";
   services.httpd = {
     enable = true;
   };
+  # boot.kernelPackages = pkgs.linuxKernel.packages.linux_rpi4;
+  boot.initrd.availableKernelModules = [ "xhci_pci" "usbhid" "usb_storage" ];
+  boot.loader.grub.enable = false;
+  boot.loader.generic-extlinux-compatible.enable = true;
+  environment.systemPackages = [ pkgs.networkmanager ];
+    home-manager = {
+    useGlobalPkgs = true;
+    useUserPackages = true;
+    backupFileExtension = ".bkp";
+  };
+  programs.git.enable = true;
+  
 }
